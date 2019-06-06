4b239c393f98077d254392db3e9cd97fe3ab35d0
RamziAgou <ramzi.agougile@edu.ece.fr>
2019-06-06 22:25:28 +0200

    Ajout des niveaux dans l'école

---
+++ ECE_School 0.41/src/controleur/DetailBulletinDAO.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+import java.sql.*;
+import modele.*;
+
+/**
+ *
+ * @author ramzi
+ */
+public class DetailBulletinDAO extends DAO<DetailBulletin>{
+
+    public DetailBulletinDAO() throws SQLException{
+        super();
+    }
+    
+    @Override
+    public boolean create(DetailBulletin a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(DetailBulletin a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(DetailBulletin a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public DetailBulletin find(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void init(Bulletin b) throws SQLException{
+        EvaluationDAO evaluationDao = new EvaluationDAO();
+        
+        stmt = connect.createStatement();   
+        Statement stmt2 = connect.createStatement();
+        Statement stmt3 = connect.createStatement();
+        
+        rset = stmt.executeQuery("SELECT * FROM detailbulletin WHERE detailbulletin.bulletin_id LIKE "+b.getId());
+        
+        while (rset.next()) {
+            String discipline = "";
+            String prof = "";
+            
+            ResultSet rset2 = stmt2.executeQuery("SELECT * FROM discipline WHERE discipline.id LIKE "
+                    + "(SELECT enseignement.discipline_id FROM enseignement WHERE enseignement.id LIKE "+rset.getInt(3)+")");
+            
+            ResultSet rset3 = stmt3.executeQuery("SELECT * FROM personne WHERE personne.id LIKE "
+                    + "(SELECT enseignement.personne_id FROM enseignement WHERE enseignement.id LIKE "+rset.getInt(3)+")");
+            
+            while(rset2.next()){
+                discipline = rset2.getString(2);
+            }
+            
+            while(rset3.next()){
+                prof = rset3.getString(2) + " " + rset3.getString(3);
+            }
+            
+            DetailBulletin db = new DetailBulletin(rset.getInt(1), rset.getString(4), prof, discipline);
+            b.ajouterDetail(db);
+            evaluationDao.init(db);
+        }
+    }
+    
+}
---
+++ ECE_School 0.41/src/controleur/PersonneDAO.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+import java.sql.*;
+import modele.*;
+
+/**
+ *
+ * @author ramzi
+ */
+public class PersonneDAO extends DAO<Personne>{
+
+    public PersonneDAO() throws SQLException{
+        super();
+    }
+    
+    @Override
+    public boolean create(Personne a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Personne a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Personne a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Personne find(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void init(Classe c) throws SQLException {
+        BulletinDAO bulletinDao = new BulletinDAO();
+        
+        stmt = connect.createStatement();   
+        Statement stmt2 = connect.createStatement();
+        Statement stmt3 = connect.createStatement();
+        
+        rset = stmt.executeQuery("SELECT * FROM personne WHERE personne.id IN "
+                + "(SELECT inscription.personne_id FROM inscription WHERE inscription.classe_id LIKE "+c.getId()+")");
+        
+        ResultSet rset2 = stmt2.executeQuery("SELECT * FROM personne WHERE personne.id IN "
+                + "(SELECT enseignement.personne_id FROM enseignement WHERE enseignement.classe_id LIKE "+c.getId()+")");
+        
+        
+        
+        while (rset.next()) {
+            Eleve e = new Eleve(rset.getInt(1), rset.getString(2), rset.getString(3));
+            c.ajouterEleve(e);
+            bulletinDao.init(e);
+            e.afficherBulletin();
+            System.out.println();
+        }
+        
+        while(rset2.next()){
+            String discipline = "";
+            ResultSet rset3 = stmt3.executeQuery("SELECT * FROM discipline WHERE discipline.id IN "
+                    + "(SELECT enseignement.discipline_id FROM enseignement WHERE enseignement.personne_id LIKE "+rset2.getInt(1)+")");
+            
+            while(rset3.next()){
+                discipline = rset3.getString(2);
+            }
+            
+            Enseignant en = new Enseignant(rset2.getInt(1), rset2.getString(2), rset2.getString(3), discipline);
+            c.ajouterProf(en);
+        }
+    }
+}
